587e578b7efb0f3c55e934f4da7780b6587b8240
Park JongIk <68894097+jongik96@users.noreply.github.com>
2022-01-19 01:41:22 +0900

    Create Implement strStr().js

---
+++ javascript/leetcode/Implement strStr().js
@@ -0,0 +1,8 @@
+var strStr = function(haystack, needle) {
+    if(niddle='') return 0;
+    if(haystack.includes(needle)){
+        return haystack.search(needle);
+    } else{
+        return -1;
+    }
+};
